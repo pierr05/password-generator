9e57d7e3813845a83bba421a13881293e95e00bc
pierr05 <pierreachlet322@gmail.com>
2022-12-29 10:48:32 -0500

    Added code to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,10 @@
-/*
-// Assignment code here
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?$#&@%!';
-  var passwordsLength = 8;
-  var random = '';
 
-  generateBtn.addEventListener("click", function() {
-    let criteriaOne = prompt("Include special character ?$#&@%! : Enter Yes or No");
-  
-    if (criteriaOne === "Yes") {
-      return characters;
-  
-    } else if (criteriaOne === "No") {
-      return characters.substring(characters.length - 7, 0);
-  
-    } else {
-      return false;
-    }
-  });
-  
-  generateBtn.addEventListener("click", function() {
-    let criteriaTwo = prompt("Include uppercase letter: Enter Yes or No");
-  
-    if (criteriaTwo === "Yes") {
-      return characters.toUpperCase();
-  
-    } else if (criteriaTwo === "No") {
-      return characters.toLowerCase();
-  
-    } else {
-      return false;
-    }
-  }); 
-  
-  generateBtn.addEventListener("click", function() {
-    let criteriaThree = prompt("Include numeric characters 0123456789 : Enter Yes or NO");
-  
-    if (criteriaThree === "Yes") {
-      return characters.replaceAll("0123456789", "")
-  
-    } else if (criteriaThree === "No") {
-      return  characters;
-  
-    } else {
-      return false;
-    }
-  }); 
-  
-  generateBtn.addEventListener("click", function() {
-    let criteriaFour = prompt("Password's length: Enter a number from 8 to 128", passwordsLength);
-  
-    if (criteriaFour > 128) {
-      alert("Please enter a valid number")
-    } 
-  });
+$(() => {
 
-  function generatePassword() {
-    for (let i = 1; i <= passwordsLength.length; i++) {
-      let randomPassword = Math.floor(Math.random() * characters.length - 0); 
-      random += characters[randomPassword]; 
-    }
-    return random;
-  }
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-*/
-
-// Rearrange 
 var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?$#&@%!';
-var passwordsLength = 8;
+var passwordsLength;
+var generateBtn = $("#generate");
+
 function generatePassword() {
 var random = '';
   for (let i = 1; i <= passwordsLength; i++) {
@@ -90,66 +14,65 @@ var random = '';
   return random;
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
 var password = generatePassword();
 var passwordText = document.querySelector("#password");
 passwordText.value = password;
-
 };
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", function() {
-  let criteriaOne = prompt("Include special character (?$#&@%!) Please enter Yes or No :");
+  generateBtn.on("click", () => {
+    let criteriaOne = prompt("Include special character (?$#&@%!) Please enter Yes or No :");
 
-  if (criteriaOne === "No") {
-    characters = characters.substring(characters.length - 7, 0)
+    if (criteriaOne === "No") {
+      characters = characters.substring(characters.length - 7, 0)
 
-  } else if (criteriaOne === "Yes") {
-    characters = characters;
+      $('#criteria-1').empty('Do not include special characters (?$#&@%!)').append('Do not include special characters (?$#&@%!)')
+  
+    } else if (criteriaOne === "Yes") {
+      characters = characters;
 
-  }
-});
+      $('#criteria-1').empty('Include special characters (?$#&@%!)').append('Include special characters (?$#&@%!)')
+    }
+  
+  // Second criteria 
+    let criteriaTwo = prompt("Include uppercase letter: Enter Yes or No");
+  
+    if (criteriaTwo === "Yes") {
+      characters = characters.toUpperCase();
 
-// Second criteria 
-generateBtn.addEventListener("click", function() {
-  let criteriaTwo = prompt("Include uppercase letter: Enter Yes or No");
+      $('#criteria-2').empty('Include uppercase letters</li>').append('Include uppercase letters')
 
-  if (criteriaTwo === "Yes") {
-    characters = characters.toUpperCase();
+    } else if (criteriaTwo === "No") {
+      characters = characters.toLowerCase();
 
-  } else if (criteriaTwo === "No") {
-    characters = characters.toLowerCase();
+      $('#criteria-2').empty('Do not include uppercase letters').append('Do not include uppercase letters')
 
-  } else {
-    characters = characters;
-  }
+    } else {
+      characters = characters;
+    }
+  
+  // Third criteria
+    let criteriaThree = prompt("Include numeric characters 0123456789 : Enter Yes or NO");
+  
+    if (criteriaThree === "No") {
+      characters = characters.replaceAll("0123456789", "");
+
+      $('#criteria-3').empty('Do not include numeric characters 0123456789').append('Do not include numeric characters 0123456789')
+
+    } else if (criteriaThree === "Yes") {
+      characters = characters;
+
+      $('#criteria-3').empty('Include numeric characters 0123456789').append('Include numeric characters 0123456789')
+    }
+
+  // Fourth criteria
+    let criteriaFour = prompt("Password's length: Please enter a number from 8 to 128");
+    passwordsLength = criteriaFour;
+
+    criteriaFour > 128 ? alert("Please enter a valid number") : $('#criteria-4').empty(`Password's length: ${passwordsLength}`).append(`Password's length: ${passwordsLength}`);
+
+    writePassword()
+  });
+
 }); 
-
-// Third criteria
-generateBtn.addEventListener("click", function() {
-  let criteriaThree = prompt("Include numeric characters 0123456789 : Enter Yes or NO");
-
-  if (criteriaThree === "No") {
-    characters = characters.replaceAll("0123456789", "")
-
-  } else if (criteriaThree === "Yes") {
-    characters = characters;
-
-  }
-}); 
-
-// Fourth criteria
-generateBtn.addEventListener("click", function() {
-  let criteriaFour = prompt("Password's length: Please enter a number from 8 to 128", passwordsLength);
-
-  if (criteriaFour > 128) {
-    alert("Please enter a valid number")
-  } 
-});
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
